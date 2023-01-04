84ec839a8fc6235e8d574432675f148eeb009463
Luis Herranz <luisherranz@gmail.com>
2023-01-04 20:02:23 +0100

    Don't run Reflect twice in getters

--- packages/deepsignal/src/index.ts
+++ packages/deepsignal/src/index.ts
@@ -49,23 +49,30 @@ const get =
 			if (fullKey === "$$") return target;
 			returnSignal = fullKey === "$length" || fullKey === "$$length";
 		}
-		const key = returnSignal ? fullKey.replace(rg, "") : fullKey;
-		let value = Reflect.get(target, key, receiver);
-		if (returnSignal && fullKey[1] === "$") return value;
 		if (!proxyToSignals.has(receiver)) proxyToSignals.set(receiver, new Map());
 		const signals = proxyToSignals.get(receiver);
-		if (!signals.has(key)) {
-			if (shouldProxy(value)) {
-				if (!objToProxy.has(value))
-					objToProxy.set(value, new Proxy(value, objectHandlers));
-				value = objToProxy.get(value);
-			}
+		const key = returnSignal ? fullKey.replace(rg, "") : fullKey;
+		if (
+			!signals.has(key) &&
+			typeof Object.getOwnPropertyDescriptor(target, key)?.get === "function"
+		) {
+			if (returnSignal && fullKey[1] === "$")
+				return Reflect.get(target, key, receiver);
 			signals.set(
 				key,
-				typeof Object.getOwnPropertyDescriptor(target, key)?.get === "function"
-					? computed(() => Reflect.get(target, key, receiver))
-					: signal(value)
+				computed(() => Reflect.get(target, key, receiver))
 			);
+		} else {
+			let value = Reflect.get(target, key, receiver);
+			if (returnSignal && fullKey[1] === "$") return value;
+			if (!signals.has(key)) {
+				if (shouldProxy(value)) {
+					if (!objToProxy.has(value))
+						objToProxy.set(value, new Proxy(value, objectHandlers));
+					value = objToProxy.get(value);
+				}
+				signals.set(key, signal(value));
+			}
 		}
 		return returnSignal ? signals.get(key) : signals.get(key).value;
 	};
