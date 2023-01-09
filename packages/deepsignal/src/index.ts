3c817c02479461c9754dc5e0def6f3bcae88884d
Luis Herranz <luisherranz@gmail.com>
2023-01-09 12:22:51 +0100

    Export everything by default

--- packages/deepsignal/src/index.ts
+++ packages/deepsignal/src/index.ts
@@ -6,4 +6,4 @@ export const useDeepSignal = <T extends object>(obj: T): DeepSignal<T> => {
 	return useMemo(() => deepSignal(obj), []);
 };
 
-export { deepSignal, type DeepSignal } from "../core/src";
+export * from "../core/src";
