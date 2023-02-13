036fd6a3fc56dc0ae0f69d54474b13f5dc1bc935
Luis Herranz <luisherranz@gmail.com>
2023-02-13 13:47:14 +0100

    Remove unnecessary signal

--- packages/deepsignal/react/src/index.ts
+++ packages/deepsignal/react/src/index.ts
@@ -1,9 +1,8 @@
-import { signal } from "@preact/signals-react";
+import "@preact/signals-react";
 import { useMemo } from "react";
 import { deepSignal, type DeepSignal } from "../../core/src";
 
 export const useDeepSignal = <T extends object>(obj: T): DeepSignal<T> => {
-	signal(0);
 	return useMemo(() => deepSignal(obj), []);
 };
 
